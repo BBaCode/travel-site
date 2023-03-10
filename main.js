fb8c110d79e97825ca3528adc97bbd6e2825d7e6
Brian Bassett <brianbassett@Brians-MacBook-Pro.local>
2023-03-10 09:43:39 -0500

    travel first commit

---
+++ main.js
@@ -0,0 +1,30 @@
+// startAnim = () => {
+//   document.querySelector(".box1").classList.toggle("blue");
+//   document.querySelector(".box2").classList.toggle("orange");
+//   document.querySelector(".box3").classList.toggle("pink");
+// };
+
+// function runIt() {
+//   let blue = document.querySelector(".box1");
+//   window.addEventListener("load", () => {
+//     setTimeout(() => {
+//       blue.classList.toggle("blue");
+//     }, 3000);
+//   });
+
+//   let orange = document.querySelector(".box2");
+//   window.addEventListener("load", () => {
+//     setTimeout(() => {
+//       orange.classList.toggle("orange");
+//     }, 8000);
+//   });
+
+//   let pink = document.querySelector(".box3");
+//   window.addEventListener("load", () => {
+//     setTimeout(() => {
+//       pink.classList.toggle("pink");
+//     }, 13000);
+//   });
+// }
+
+// runIt();
